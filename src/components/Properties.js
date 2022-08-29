fc1b7bf2b1931b536de81cb732650bb7a426e0f1
UtkarshDixit-py <utkarshdixit303@gmail.com>
2022-08-29 14:42:09 +0530

    undo semantic UI

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -7,18 +7,20 @@ const Properties = () => {
   const PropList = useSelector((state) => state.userReducer.List);
 
   return (
-    // <Container>
-    //   {PropList.map((obj, i) => (
-    //     <PropertyItem {...obj} key={i} />
-    //   ))}
-    // </Container>
     <Container>
-      <div class="ui three link cards">
-        {PropList.map((obj, i) => (
-          <PropertyItem {...obj} key={i} />
-        ))}
-      </div>
+      {PropList.map((obj, i) => (
+        <PropertyItem {...obj} key={i} />
+      ))}
     </Container>
+
+    // SEMANTIC UI
+    // <Container>
+    //   <div class="ui three link cards">
+    //     {PropList.map((obj, i) => (
+    //       <PropertyItem {...obj} key={i} />
+    //     ))}
+    //   </div>
+    // </Container>
   );
 };
 
@@ -29,44 +31,48 @@ function PropertyItem({ name, id, imgUrl, address, beds, baths, area }) {
   }
 
   return (
-    // <PropertyContainer>
-    //   <p>{name}</p>
-    //   {/* <img src={imgUrl} /> */}
-    //   <button onClick={() => handleAddToFav(id)}>Add to favourites</button>
-    // </PropertyContainer>
-    <div class="card">
-      <div class="image">
-        <img src={imgUrl} />
-      </div>
-      <div class="content">
-        <div class="header">{name}</div>
-        <div class="meta">
-          <i class="right floated like big icon"></i>
-        </div>
-        <div class="description">M{address}</div>
-      </div>
-      <div class="extra content">
-        <span class="right floated">Area : {area}</span>
-        <span>
-          <i class="bed icon"></i>
-          {beds} beds &amp; {baths} baths
-        </span>
-      </div>
-    </div>
+    <PropertyContainer>
+      <p>{name}</p>
+      {/* <img src={imgUrl} /> */}
+      <button onClick={() => handleAddToFav(id)}>Add to favourites</button>
+    </PropertyContainer>
+
+    // SEMANTIC UI
+    // <div class="card">
+    //   <div class="image">
+    //     <img src={imgUrl} />
+    //   </div>
+    //   <div class="content">
+    //     <div class="header">{name}</div>
+    //     <div class="meta">
+    //       <i class="right floated like big icon"></i>
+    //     </div>
+    //     <div class="description">M{address}</div>
+    //   </div>
+    //   <div class="extra content">
+    //     <span class="right floated">Area : {area}</span>
+    //     <span>
+    //       <i class="bed icon"></i>
+    //       {beds} beds &amp; {baths} baths
+    //     </span>
+    //   </div>
+    // </div>
   );
 }
 
 const Container = styled.div`
-  /* border: 2px solid red;
+  border: 2px solid red;
   display: flex;
   grid-template-columns: auto auto auto;
   row-gap: 12px;
-  gap: 25px; */
+  gap: 25px;
+
+
   padding: 50px 2px;
 `;
 
 const PropertyContainer = styled.div`
-  border: 1px solid black;
+  /* border: 1px solid black;
   height: 150px;
   padding: 20px;
   font-size: 30px;
@@ -75,7 +81,7 @@ const PropertyContainer = styled.div`
   img {
     height: 100px;
     width: inherit;
-  }
+  } */
 `;
 
 export default Properties;
